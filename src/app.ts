f726799a84ecda0bfd8956b82c23d20c0d05f7df
Dineshreddyvengampalli <dineshreddyvengampalli@gmail.com>
2025-01-28 20:38:12 +0530

    api-init commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import bodyParser from "body-parser";
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res)=> {
+    res.send('Hi from express')
+})
+
+
+export default app
